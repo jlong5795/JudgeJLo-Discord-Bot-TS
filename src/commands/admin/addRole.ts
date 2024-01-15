a5b12823f56e18d12e0827650b66252817714596
Jason Long <jasonlong5795@gmailcom>
2024-01-15 10:25:47 -0600

    Stubbed out manual role management

---
+++ src/commands/admin/addRole.ts
@@ -0,0 +1,20 @@
+import type { CommandData, SlashCommandProps, CommandOptions } from "commandkit";
+
+export const data: CommandData = {
+    name: "Add Role",
+    description: "Manually add a role to a user - Admin Access Only",
+}
+
+export function run({ interaction, client, handler }: SlashCommandProps) {
+    // Parse the role to be added - do we use buttons here
+    // Parse the user to receive the new role
+    // Add the role to the user
+    interaction.reply(`Successsfully added role to user.`);
+}
+
+export const options: CommandOptions = {
+    devOnly: true,
+    userPermissions: ["Administrator", "AddReactions"],
+    botPermissions: ["Administrator", "AddReactions"],
+    deleted: false
+}
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,4 @@
 import type { Client } from "discord.js";
-import type { CommandKit } from "commandkit";
 
 export default function (client: Client) {
     console.log(`✅ ${client.user?.username} is online ✅`);
