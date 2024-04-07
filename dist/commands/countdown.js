c78bf6ef4b2a730ee9930a286ed3b8cc72fb6e49
Jason Long <jasonlong5795@gmailcom>
2024-04-06 23:27:33 -0500

    Update countdown command

--- dist/commands/countdown.js
+++ dist/commands/countdown.js
@@ -1,19 +1,32 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.options = exports.run = exports.data = void 0;
+const dayjs_1 = __importDefault(require("dayjs"));
+const duration_1 = __importDefault(require("dayjs/plugin/duration"));
+const timezone_1 = __importDefault(require("dayjs/plugin/timezone"));
+const utc_1 = __importDefault(require("dayjs/plugin/utc"));
+dayjs_1.default.extend(duration_1.default);
+dayjs_1.default.extend(timezone_1.default);
+dayjs_1.default.extend(utc_1.default);
 exports.data = {
     name: "countdown",
     description: "Countdown to Sky Vaults Launch Date",
 };
 const countdown = () => {
-    const launchDate = new Date("2024-04-07");
-    const currentDate = new Date();
-    const timeDiff = launchDate.getTime() - currentDate.getTime();
-    const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return days;
+    const launchDate = dayjs_1.default.tz("2024-04-07T18:00:00", "America/Chicago");
+    const currentDate = (0, dayjs_1.default)().tz("America/Chicago");
+    const timeDiff = dayjs_1.default.duration(launchDate.diff(currentDate));
+    const days = timeDiff.days();
+    const hours = timeDiff.hours();
+    const minutes = timeDiff.minutes();
+    const seconds = timeDiff.seconds();
+    return `The Sky Vaults server will be launching in ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds! If you're not there, what are you doing with your life?`;
 };
 function run({ interaction, client, handler }) {
-    interaction.reply(`Sky Vaults will be launching in ${countdown()} days!`);
+    interaction.reply(countdown());
 }
 exports.run = run;
 exports.options = {
--- src/commands/countdown.ts
+++ src/commands/countdown.ts
@@ -1,4 +1,12 @@
 import type { CommandData, SlashCommandProps, CommandOptions } from "commandkit";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(duration);
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const data: CommandData = {
     name: "countdown",
@@ -6,15 +14,19 @@ export const data: CommandData = {
 }
 
 const countdown = () => { 
-    const launchDate = new Date("2024-04-07");
-    const currentDate = new Date();
-    const timeDiff = launchDate.getTime() - currentDate.getTime();
-    const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    return days;
+    const launchDate = dayjs.tz("2024-04-07T18:00:00", "America/Chicago");
+    const currentDate = dayjs().tz("America/Chicago");
+    const timeDiff = dayjs.duration(launchDate.diff(currentDate));
+    const days = timeDiff.days();
+    const hours = timeDiff.hours();
+    const minutes = timeDiff.minutes();
+    const seconds = timeDiff.seconds();
+
+    return `The Sky Vaults server will be launching in ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds! If you're not there, what are you doing with your life?`;
 }
 
 export function run({ interaction, client, handler }: SlashCommandProps) {
-    interaction.reply(`Sky Vaults will be launching in ${countdown()} days!`);
+    interaction.reply(countdown());
 }
 
 export const options: CommandOptions = {
